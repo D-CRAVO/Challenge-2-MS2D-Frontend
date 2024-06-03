610c142f21f6abeff3e00fde4706404ea09a8057
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-06-03 16:35:13 +0200

    Implémentation de la liste des groupes

---
+++ src/app/components/group-list/group-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { GroupService } from '../../services/group.service';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-group-list',
+  templateUrl: './group-list.component.html',
+  styleUrl: './group-list.component.css',
+})
+export class GroupListComponent implements OnInit, OnDestroy{
+
+  groups: any
+  subscription: Subscription
+
+  constructor(
+    private groupService: GroupService
+  ){
+    this.subscription = new Subscription
+  }
+
+  ngOnInit(): void {
+      this.subscription = this.groupService.getGroups().subscribe(groups => this.groups = groups);
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe
+  }
+}

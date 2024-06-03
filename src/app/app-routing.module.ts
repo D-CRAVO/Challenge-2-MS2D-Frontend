8fd0f3052a8475890572677608701810c4a7e0a1
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-06-03 08:14:00 +0200

    Nettoyage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LearnerListComponent } from './components/learner-list/learner-list.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'learners', component: LearnerListComponent},
+  {path: 'accueil', component: AppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,31 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MenuComponent } from './components/menu/menu.component';
 
-import {MatMenuModule} from '@angular/material/menu'; 
+import { MenuComponent } from './components/menu/menu.component';
+import { LearnerListComponent } from './components/learner-list/learner-list.component';
+
+import { MatMenuModule } from '@angular/material/menu'; 
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { WelcomeComponent } from './components/welcome/welcome.component'; 
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    LearnerListComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     MatMenuModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    AppRoutingModule,
   ],
   providers: [
     provideClientHydration(),
